35d4f8b46872d57015118401edc9dd034eab4b16
deepika57 <amruthadeepika@gmail.com>
2022-02-27 19:06:01 +0100

    push func done

--- Assignment1.js
+++ Assignment1.js
@@ -8,4 +8,15 @@ class Stack {
     this.upper = -1;
     this.size = size;
   }
+
+  push(value) {
+    if (this.upper == this.size - 1) {
+      console.log("Stack overflow so can't be Inserted !!! ");
+    } else {
+      this.upper = this.upper + 1;
+      this.array[this.upper] = value;
+      console.log(`Added element to the stack is ${value}`);
+      return this.upper;
+    }
+  }
 }
